d762c5b7d51333cc201c46eb75d2df67a88cb17b
PUG <kjl2875@gmail.com>
2017-11-05 12:45:46 +0900

    Version 1.0 Release

--- PrimeNumbers/EnumPrimeNumbers/EnumPrimeNumbers.c
+++ PrimeNumbers/EnumPrimeNumbers/EnumPrimeNumbers.c
@@ -2,6 +2,8 @@
  * EnumPrimeNumbers.c
  * 2017-11-04
  * PrimeNumbers 순서대로 출력
+ * 최대 소수크기 한계점과 연산속도의 과제가 남아있음.
+ * Windows Visual Studio 2015 Community Required
  */
 
 #define IMPL_003
@@ -26,9 +28,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <limits.h>
 
 //#define MEMORY_MAX_ADDRESS ULLONG_MAX
 #define MEMORY_MAX_ADDRESS (1024*1024*1024)
+#define PREOGRAM_VERSION "1.0"
 #endif
 
 #if defined IMPL_001
@@ -37,22 +41,26 @@
 	void impl(void);
 #elif defined IMPL_003
 	int impl(void);
-	errno_t loadFile(const size_t *const c, const unsigned long long int *const m, const unsigned long long int *const p);
-	errno_t saveFile(const size_t *const c, const unsigned long long int *const m, const unsigned long long int *const p);
+	void uiHelp(void);
+	errno_t saveFile(const int *const n, const int *const c, const unsigned long long int *const m, const unsigned long long int *const p);
+	errno_t loadFile(const int *n, const int *c, const unsigned long long int *m, const unsigned long long int *p);
 #endif
 
 
 int main()
 {
-	return impl();
+	int code = impl();
+	return code;
 }
 
 #ifdef IMPL_003
+
 int impl(void)
 {
 	FILE *fp;
 	unsigned long long int n = 2;
-	size_t c = 0;
+	signed int c = 0;
+	// size_t c = 0;
 	// size_t i = 0; // error C3016: 'i': OpenMP 'for' 문의 인덱스 변수는 부호 있는 정수 계열 형식이어야 합니다.
 	signed int i = 0;
 	unsigned long long int *m, *p;
@@ -61,6 +69,13 @@ int impl(void)
 
 	m = malloc(sizeof(unsigned long long int) * MEMORY_MAX_ADDRESS - 1);
 	p = malloc(sizeof(unsigned long long int) * MEMORY_MAX_ADDRESS);
+
+	if (INT_MAX < MEMORY_MAX_ADDRESS)
+	{
+		printf("Error: MEMORY_ADDRESS_OVERFLOW: MAX_VALUE: %d\n", INT_MAX);
+		return 1;
+	}
+	
 	if (m == NULL || p == NULL)
 	{
 		printf("Error: Out of memory\n");
@@ -68,11 +83,7 @@ int impl(void)
 		return 1;
 	}
 
-	printf("Check number: 'c'\n");
-	printf("Save to file: 's'\n");
-	printf("Load from file: 'l'\n");
-	printf("Exit program: 'e'\n");
-	printf("Program is running...\n");
+	uiHelp();
 
 	while (c < MEMORY_MAX_ADDRESS && inturrpt == 0)
 	{
@@ -81,16 +92,24 @@ int impl(void)
 			switch (_getch())
 			{
 			case 'c':
-				printf("%llu\n", p[c - 1]);
+				printf("%d Primes, Max number %llu\n", c, p[c - 1]);
 				break;
 			case 'e':
 				inturrpt = 1;
 				break;
 			case 's':
-				saveFile(&c, m ,p);
+				fflush(stdin);
+				printf("c: %d\nm[%d]: %llu\np[%d]: %llu\n", c, c-1, m[c - 1], c-1, p[c - 1]);
+				saveFile(&n, &c, m ,p);
 				break;
 			case 'l':
-				loadFile(&c, m, p);
+				fflush(stdin);
+				printf("c: %d\nm[%d]: %llu\np[%d]: %llu\n", c, c - 1, m[c - 1], c - 1, p[c - 1]);
+				loadFile(&n, &c, m, p);
+				printf("c: %d\nm[%d]: %llu\np[%d]: %llu\n", c, c - 1, m[c - 1], c - 1, p[c - 1]);
+				continue;
+			case 'h':
+				uiHelp();
 				break;
 			}
 		}
@@ -114,8 +133,11 @@ int impl(void)
 		n++;
 	}
 
+	if (c == INT_MAX)
+		printf("Prime check number is maximun: %d\n", c);
+
 	printf("%llu\n", p[c-1]);
-	saveFile(&c, m, p);
+	saveFile(&n, &c, m, p);
 
 	free(m);
 	free(p);
@@ -123,21 +145,31 @@ int impl(void)
 	return 0;
 }
 
-errno_t saveFile(const size_t *const c, const unsigned long long int *const m, const unsigned long long int *const p)
+void uiHelp(void)
+{
+	printf("Enum PrimeNumbers v%s\n", PREOGRAM_VERSION);
+	printf("Check number: 'c'\n");
+	printf("Save to file: 's'\n");
+	printf("Load from file: 'l'\n");
+	printf("Exit program: 'e'\n");
+	printf("Program is running...\n");
+}
+
+errno_t saveFile(const int *const n, const int *const c, const unsigned long long int *const m, const unsigned long long int *const p)
 {
 	char fpath[256];
 	FILE *fp;
-	size_t i;
+	int i;
 	errno_t errno;
 
 	printf("path: ");
-	gets_s(fpath, sizeof(char) * sizeof(fpath));
+	scanf_s("%s", fpath);
 	printf("path: %s: ", fpath);
 
 	switch (errno = fopen_s(&fp, fpath, "w"))
 	{
 	case 0:
-		fprintf_s(fp, "%llu\n", *c);
+		fprintf_s(fp, "%d %d\n", *c, *n);
 
 		for (i = 0; i < *c; i++)
 			fprintf_s(fp, "%llu ", m[i]);
@@ -159,21 +191,21 @@ errno_t saveFile(const size_t *const c, const unsigned long long int *const m, c
 	return errno;
 }
 
-errno_t loadFile(const size_t *c, const unsigned long long int *m, const unsigned long long int *p)
+errno_t loadFile(const int *n, const int *c, const unsigned long long int *m, const unsigned long long int *p)
 {
 	char fpath[256];
 	FILE *fp;
-	size_t i;
+	int i;
 	errno_t errno;
 
 	printf("path: ");
-	gets_s(fpath, sizeof(char) * sizeof(fpath));
+	scanf_s("%s", fpath);
 	printf("path: %s: ", fpath);
 
 	switch (errno = fopen_s(&fp, fpath, "r"))
 	{
 	case 0:
-		fscanf_s(fp, "%llu\n", c);
+		fscanf_s(fp, "%d %d\n", c, n);
 
 		for (i = 0; i < *c; i++)
 			fscanf_s(fp, "%llu ", &m[i]);
